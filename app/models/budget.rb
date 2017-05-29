b75dcf1bc8baa5bb480b3c2bcaf0c9072960289e
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-29 08:06:37 -0300

    Add tax calculation to the model on before_save

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,15 @@
 class Budget < ApplicationRecord
+  before_save :calculate_tax
+
+  def calculate_tax
+    if self.days_until_due_date % 10 === 0
+      percent_tax = 0.0165 * (self.days_until_due_date / 10)
+    else
+      percent_tax = 0.0165 * ((self.days_until_due_date / 10) + 1)
+    end
+    self.tax_value = self.duplicate_value * percent_tax
+    self.value_to_pay = self.duplicate_value - self.tax_value
+  end
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :requester_email, presence: true, format: { with: VALID_EMAIL_REGEX }
