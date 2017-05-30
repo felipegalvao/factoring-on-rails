1ad80fae23d87535037112bda8cc55bc2d1b725f
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-30 00:43:05 -0300

    Add test for Budgets Controller

--- test/controllers/budgets_controller_test.rb
+++ test/controllers/budgets_controller_test.rb
@@ -7,13 +7,21 @@ class BudgetsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get show" do
-    get budgets_show_url
+    @budget = budgets(:one)
+    get budget_path(@budget)
     assert_response :success
   end
 
-  test "should get create" do
-    get budgets_create_url
-    assert_response :success
+  test "post for budget creation url should redirect to Budget" do
+    post budgets_create_url, params: { budget: {
+      requester_name: "Requester Name",
+      requester_email: "requester@email.com",
+      company_name: "Company Name",
+      monthly_revenue: 15000,
+      duplicate_value: 3000,
+      days_until_due_date: 10
+    }}
+    assert_redirected_to budget_path(Budget.last)
   end
 
 end
