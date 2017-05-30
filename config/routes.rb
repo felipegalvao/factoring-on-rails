8ffb0d782fe6d6126af46cf12d736c7d3a00888d
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-30 00:43:41 -0300

    Add names to some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'budgets#new'
-  post '/', to: 'budgets#create'
+  root 'budgets#new', as: :budgets_new
+  post '/', to: 'budgets#create', as: :budgets_create
   get '/:id', to: 'budgets#show', as: :budget
 end
